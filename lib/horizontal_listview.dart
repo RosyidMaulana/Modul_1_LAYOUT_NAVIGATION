d6348c870aa9b5ed2ec2d0c81366b50ad9d7cbc0
RosyidMaulana <ocidbang728@gmail.com>
2025-04-12 10:43:27 +0700

    first commit 2.0

---
+++ lib/horizontal_listview.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HorizontalListViewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Horizontal ListView')),
+      body: Container(
+        height: 120,
+        child: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return Container(
+              width: 100,
+              margin: EdgeInsets.all(8),
+              color: Colors.blue,
+              child: Center(child: Text('Item $index')),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
